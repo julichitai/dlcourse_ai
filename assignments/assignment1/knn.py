2b38726081095faf670695db2ac415a6c0f833de
julichitai <chichb1627>
2020-07-12 14:56:03 +0500

    implemented k-nearest neighbors in knn.py

--- assignments/assignment1/knn.py
+++ assignments/assignment1/knn.py
@@ -54,8 +54,8 @@ class KNN:
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
             for i_train in range(num_train):
-                # TODO: Fill dists[i_test][i_train]
-                pass
+                dists[i_test][i_train] = np.sum(np.abs(X[i_test] - self.train_X[i_train]))
+        return dists
 
     def compute_distances_one_loop(self, X):
         '''
@@ -73,9 +73,8 @@ class KNN:
         num_test = X.shape[0]
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
-            # TODO: Fill the whole row of dists[i_test]
-            # without additional loops or list comprehensions
-            pass
+            dists[i_test] = np.sum(np.abs(X[0] - self.train_X), axis=1)
+        return dists
 
     def compute_distances_no_loops(self, X):
         '''
@@ -91,10 +90,11 @@ class KNN:
         '''
         num_train = self.train_X.shape[0]
         num_test = X.shape[0]
-        # Using float32 to to save memory - the default is float64
+        # Using float32 to save memory - the default is float64
         dists = np.zeros((num_test, num_train), np.float32)
+        dists = np.sum(np.abs(X[:, np.newaxis] - self.train_X), axis=2)
         # TODO: Implement computing all distances with no loops!
-        pass
+        return dists
 
     def predict_labels_binary(self, dists):
         '''
@@ -113,7 +113,10 @@ class KNN:
         for i in range(num_test):
             # TODO: Implement choosing best class based on k
             # nearest training samples
-            pass
+            dists_sample = [s for s in enumerate(dists[i])]
+            k_close = sorted(dists_sample, key=lambda x: x[1])[:self.k]
+            k_close_labels = [self.train_y[s[0]] for s in k_close]
+            pred[i] = max(set(k_close_labels), key=lambda x: k_close_labels.count(x))
         return pred
 
     def predict_labels_multiclass(self, dists):
@@ -129,10 +132,10 @@ class KNN:
            for every test sample
         '''
         num_test = dists.shape[0]
-        num_test = dists.shape[0]
         pred = np.zeros(num_test, np.int)
         for i in range(num_test):
-            # TODO: Implement choosing best class based on k
-            # nearest training samples
-            pass
+            dists_sample = [s for s in enumerate(dists[i])]
+            k_close = sorted(dists_sample, key=lambda x: x[1])[:self.k]
+            k_close_labels = list(self.train_y[[s[0] for s in k_close]])
+            pred[i] = max(set(k_close_labels), key=lambda x: k_close_labels.count(x))
         return pred
